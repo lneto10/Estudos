47458126f55cb7ddfaa3b8e5db80fb15c92a3169
Luiz Neto <luiznetodw@gmail.com>
2022-09-21 21:44:55 -0300

    Termino do curso de React Native na Alura

--- PROGRAMACAO PARA DISPOSITIVOS MOVEIS/orgs-cesta/App.js
+++ PROGRAMACAO PARA DISPOSITIVOS MOVEIS/orgs-cesta/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import { StatusBar,SafeAreaView, StyleSheet, View } from 'react-native';
 import { useFonts, Montserrat_400Regular, Montserrat_700Bold, Montserrat_400 } from '@expo-google-fonts/montserrat';
+
+import AppLoading from 'expo-app-loading';
+
 import Cesta from './src/telas/Cesta';
+import mock from './src/mocks/cesta';
 
 export default function App() {
   const[fonteCarregada] = useFonts({
@@ -9,13 +13,14 @@ export default function App() {
     "montserratBold": Montserrat_700Bold,
   });
 
+
   if (!fonteCarregada){
-    return <View />
+    return <AppLoading />
   }
   return (
-    <SafeAreaView>
+    <SafeAreaView style={{flex: 1}}>
       <StatusBar />
-      <Cesta />
+      <Cesta {...mock}/>
     </SafeAreaView>
   );
 }
