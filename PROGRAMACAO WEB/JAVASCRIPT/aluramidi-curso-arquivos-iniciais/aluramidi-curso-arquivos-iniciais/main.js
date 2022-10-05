12f83402574f402887c2d4746700c15817187e84
Luiz Neto <luiznetodw@gmail.com>
2022-10-04 21:12:31 -0300

    Curso de Criando paginas dinamicas com Javascript

---
+++ PROGRAMACAO WEB/JAVASCRIPT/aluramidi-curso-arquivos-iniciais/aluramidi-curso-arquivos-iniciais/main.js
@@ -0,0 +1,11 @@
+function tocaPom (){
+    document.querySelector('#som_tecla_pom').play();
+}
+
+
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+listaDeTeclas[0].onclick = tocaPom;
+
+
