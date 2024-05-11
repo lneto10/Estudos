e72452d19222bbc44a0f11cab6b87ef8e033e815
lneto10 <luiznetodw@gmail.com>
2024-05-11 15:52:58 -0300

    Exercicios de fixacao

---
+++ JAVA-Udemy/Seção 10 - Comportamento de memórias,arrays,listas/ex-fixacao/ex1/src/program/EX1.java
@@ -0,0 +1,20 @@
+package program;
+
+import java.util.Scanner;
+import java.util.Locale;
+/*
+* 
+* */
+public class Application {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.us);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Quantos numeros você vai digitar: ");
+        int n = sc.nextInt();
+        int vet[] = new int[n];
+
+
+
+    }
+}
