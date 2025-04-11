584044121295439a8458579b684d479817e67dd1
Luiz Neto <105994182+lneto10@users.noreply.github.com>
2025-04-11 05:53:41 -0300

    Solução ruim

--- JAVA-Udemy/Seção 14 - Tratamento de Exceções/exemplo2/excecoes2/src/application/Program.java
+++ JAVA-Udemy/Seção 14 - Tratamento de Exceções/exemplo2/excecoes2/src/application/Program.java
@@ -33,18 +33,14 @@ public class Program {
             System.out.println("Check out date (dd/MM/yyyy)");
             checkOut = sdf.parse(sc.next());
 
-            Date now = new Date();
-            
-            if(checkIn.before(now) || checkOut.before(now)){
-                System.out.println("Error in reservation: Reservation dates for update must be future");
-            }
-            else if (!checkOut.after(checkIn)) {
-                System.out.println("Error in reservation: Reservation dates for update must be future");
+            String error = reservation.updateDates(checkIn, checkOut);
+            if( error != null){
+                System.out.println("Error in reservation "+error);
             }
             else {
-                reservation.updateDates(checkIn, checkOut);
-                System.out.println(" Reservation "+reservation);
+                System.out.println("Reservation "+reservation);
             }
+            
         }
 
 
