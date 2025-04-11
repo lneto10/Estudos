c2a8f34e4260111846ac28f5b8f5fdd3107b0e6c
Luiz Neto <105994182+lneto10@users.noreply.github.com>
2025-04-11 06:40:01 -0300

    solucao boa

--- JAVA-Udemy/Seção 14 - Tratamento de Exceções/exemplo2/excecoes2/src/model/entities/Reservation.java
+++ JAVA-Udemy/Seção 14 - Tratamento de Exceções/exemplo2/excecoes2/src/model/entities/Reservation.java
@@ -4,6 +4,8 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
+import model.exceptions.DomainException;
+
 public class Reservation {
 
     private Integer roomNumber;
@@ -41,19 +43,18 @@ public class Reservation {
         return TimeUnit.DAYS.convert(diff,TimeUnit.MILLISECONDS);
     }
 
-    public String updateDates(Date checkIn, Date checkOut){
+    public void updateDates(Date checkIn, Date checkOut) throws DomainException {
 
         Date now = new Date();
             
         if(checkIn.before(now) || checkOut.before(now)){
-           return "Error in reservation: Reservation dates for update must be future";
+           throw  new DomainException("Reservation dates for update must be future");
         }
         if (!checkOut.after(checkIn)) {
-            return "Check-out date must be after check-in date: ";
+            throw  new DomainException ("Check-out date must be after check-in date: ");
         }
         this.checkIn = checkIn;
         this.checkOut = checkOut;
-        return null;
 
     }
 
